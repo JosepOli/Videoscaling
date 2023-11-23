b150f387f0f6f281324be51dcf29180f1d5c6bdb
Josep Olivé <josepolive89@gmail.com>
2023-11-23 18:52:45 +0100

    Rafactored and improved, added error handling

--- upscaler.py
+++ upscaler.py
@@ -3,84 +3,183 @@ from tkinter import filedialog
 import subprocess
 import os
 import shutil
+import re
+from typing import List, Optional
+import threading
+import logging
 
-def select_files_or_folder():
-    """
-    Opens a dialog to select either a single video file or a folder containing videos.
-    """
-    root = tk.Tk()
-    root.withdraw()
-    root.title("Select Video Files or Folder")
-    file_path = filedialog.askopenfilename(title="Select a Video File", filetypes=[("Video files", "*.mp4 *.avi *.mkv")])
-    if file_path:
-        return [file_path]
-    folder_path = filedialog.askdirectory(title="Select Folder Containing Videos")
-    if folder_path:
-        return [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith(('.mp4', '.avi', '.mkv'))]
-    return []
+# Initialize logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
-def select_destination_folder():
-    """
-    Opens a dialog to select the destination folder for upscaled videos.
-    """
-    root = tk.Tk()
-    root.withdraw()
-    root.title("Select Destination Folder")
-    folder_path = filedialog.askdirectory(title="Select Destination Folder for Upscaled Videos")
-    return folder_path if folder_path else "upscaled_videos"
 
-def extract_frames(video_file, destination_folder):
-    """
-    Extracts frames from the given video file using CPU-based extraction.
-    Addresses the deprecated -vsync parameter.
-    """
-    frame_folder = os.path.join(destination_folder, "temporary_frames")
-    os.makedirs(frame_folder, exist_ok=True)
+class VideoProcessor:
+    def __init__(self):
+        self.destination_folder = self.select_destination_folder()
+        os.makedirs(self.destination_folder, exist_ok=True)
+        self.video_files = self.select_files_or_folder()
 
-    # Updated FFmpeg command for CPU-based frame extraction
-    ffmpeg_command = [
-        "ffmpeg", "-i", video_file,
-        "-vf", "fps=fps=23.98",  # Set this to match the source video's fps
-        os.path.join(frame_folder, "frame_%04d.png")
-    ]
-    subprocess.run(ffmpeg_command)
-    return frame_folder
+    @staticmethod
+    def run_subprocess(
+        command: List[str], capture_output: bool = False
+    ) -> subprocess.CompletedProcess:
+        """
+        Runs a subprocess with the given command and optional output capture.
+        """
+        try:
+            return subprocess.run(command, text=True, capture_output=capture_output)
+        except subprocess.SubprocessError as e:
+            logging.error(f"Error running command {' '.join(command)}: {e}")
+            return None
 
-def upscale_frames(frame_folder):
-    """
-    Upscales the frames using realesrgan-ncnn-vulkan.
-    Uses NVENC for GPU-accelerated upscaling.
-    """
-    output_folder = os.path.join(os.path.dirname(frame_folder), "out_frames")
-    os.makedirs(output_folder, exist_ok=True)
-    subprocess.run(["./realesrgan-ncnn-vulkan.exe", "-i", frame_folder, "-o", output_folder, "-n", "realesr-animevideov3", "-s", "2", "-f", "png"])
-    shutil.rmtree(frame_folder)
-    return output_folder
+    @staticmethod
+    def get_frame_rate(video_file: str) -> str:
+        """
+        Extracts the frame rate of the given video file using FFmpeg.
+        """
+        result = VideoProcessor.run_subprocess(
+            ["ffmpeg", "-i", video_file], capture_output=True
+        )
+        if result and result.stderr:
+            matches = re.search(r"(\d{1,3}\.\d{1,2}) fps", result.stderr)
+            return (
+                matches.group(1) if matches else "25"
+            )  # Default to 25 fps if not detected
+        return "25"
 
-def reassemble_video(original_video, frame_folder, destination_folder):
-    """
-    Reassembles the video from the upscaled frames and original audio.
-    Uses NVENC for GPU-accelerated video encoding.
-    """
-    upscaled_video = os.path.join(destination_folder, "upscaled_" + os.path.basename(original_video))
-    ffmpeg_command = [
-        "ffmpeg", "-i", os.path.join(frame_folder, "frame_%04d.png"), 
-        "-i", original_video, "-c:v", "h264_nvenc", "-preset", "fast", 
-        "-r", "25", "-pix_fmt", "yuv420p", "-vf", "scale=in_range=jpeg:out_range=mpeg", 
-        upscaled_video
-    ]
-    subprocess.run(ffmpeg_command)
-    shutil.rmtree(frame_folder)
+    @staticmethod
+    def select_files_or_folder() -> List[str]:
+        """
+        Opens a dialog to select either a single video file or a folder containing videos.
+        """
+        root = tk.Tk()
+        root.withdraw()
+        file_path = filedialog.askopenfilename(
+            title="Select a Video File",
+            filetypes=[("Video files", "*.mp4 *.avi *.mkv")],
+        )
+        if file_path:
+            return [file_path]
+        folder_path = filedialog.askdirectory(title="Select Folder Containing Videos")
+        if folder_path:
+            return [
+                os.path.join(folder_path, f)
+                for f in os.listdir(folder_path)
+                if f.endswith((".mp4", ".avi", ".mkv"))
+            ]
+        return []
+
+    @staticmethod
+    def select_destination_folder() -> str:
+        """
+        Opens a dialog to select the destination folder for upscaled videos.
+        """
+        root = tk.Tk()
+        root.withdraw()
+        folder_path = filedialog.askdirectory(
+            title="Select Destination Folder for Upscaled Videos"
+        )
+        return folder_path if folder_path else "upscaled_videos"
+
+    def process_video(self, video_file: str):
+        """
+        Process a single video file.
+        """
+        logging.info(f"Processing {video_file}...")
+        frame_folder = self.extract_frames(video_file)
+        self.upscale_frames(frame_folder)
+        self.reassemble_video(video_file, frame_folder)
+
+    def extract_frames(self, video_file: str) -> str:
+        """
+        Extracts frames from the given video file using the detected frame rate.
+        """
+        frame_folder = os.path.join(self.destination_folder, "temporary_frames")
+        os.makedirs(frame_folder, exist_ok=True)
+
+        frame_rate = self.get_frame_rate(video_file)
+
+        ffmpeg_command = [
+            "ffmpeg",
+            "-i",
+            video_file,
+            "-vf",
+            f"fps=fps={frame_rate}",
+            "-q:v",
+            "2",  # Quality scale for JPEG
+            os.path.join(frame_folder, "frame_%04d.jpg"),
+        ]
+        self.run_subprocess(ffmpeg_command)
+        return frame_folder
+
+    def upscale_frames(self, frame_folder: str):
+        """
+        Upscales the frames using realesrgan-ncnn-vulkan.
+        """
+        output_folder = os.path.join(os.path.dirname(frame_folder), "out_frames")
+        os.makedirs(output_folder, exist_ok=True)
+
+        # Path to the realesrgan-ncnn-vulkan executable
+        realesrgan_executable = os.path.join(
+            "Real-ESRGAN", "realesrgan", "realesrgan-ncnn-vulkan.exe"
+        )
+
+        self.run_subprocess(
+            [
+                realesrgan_executable,
+                "-i",
+                frame_folder,
+                "-o",
+                output_folder,
+                "-n",
+                "realesr-animevideov3",
+                "-s",
+                "2",
+                "-f",
+                "png",
+            ]
+        )
+        
+    def reassemble_video(self, original_video: str, frame_folder: str):
+        """
+        Reassembles the video from the upscaled frames and original audio.
+        """
+        upscaled_video = os.path.join(
+            self.destination_folder, "upscaled_" + os.path.basename(original_video)
+        )
+        frame_rate = self.get_frame_rate(original_video)
+        ffmpeg_command = [
+            "ffmpeg",
+            "-i",
+            os.path.join(frame_folder, "frame_%04d.png"),
+            "-i",
+            original_video,
+            "-c:v",
+            "h264_nvenc",
+            "-preset",
+            "fast",
+            "-r",
+            frame_rate,
+            "-pix_fmt",
+            "yuv420p",
+            upscaled_video,
+        ]
+        self.run_subprocess(ffmpeg_command)
+        shutil.rmtree(frame_folder)
+
+    def run(self):
+        """
+        Process all selected video files.
+        """
+        for video_file in self.video_files:
+            self.process_video(video_file)
+
 
 def main():
-    destination_folder = select_destination_folder()
-    os.makedirs(destination_folder, exist_ok=True)
-    video_files = select_files_or_folder()
-    for video_file in video_files:
-        print(f"Processing {video_file}...")
-        frame_folder = extract_frames(video_file, destination_folder)
-        upscale_frames(frame_folder)
-        reassemble_video(video_file, frame_folder, destination_folder)
+    video_processor = VideoProcessor()
+    video_processor.run()
+
 
 if __name__ == "__main__":
     main()
