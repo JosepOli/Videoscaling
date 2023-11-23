16afeac696d0c8ad9b24a41406d6a98854b449cb
Josep Olivé <josepolive89@gmail.com>
2023-11-23 22:55:15 +0100

    refactoring and repurposing, separating concerns for better scalability

--- gui.py
+++ gui.py
@@ -5,9 +5,45 @@ from PyQt5.QtWidgets import (
     QWidget,
     QLabel,
     QFileDialog,
+    QProgressBar,
+    QApplication,
+    QListWidget,
 )
+from PyQt5.QtCore import Qt
 from video_processor import VideoProcessor
-import os
+import sys
+
+
+class FileListWidget(QListWidget):
+    def __init__(self, parent=None):
+        super(FileListWidget, self).__init__(parent)
+        self.setAcceptDrops(True)
+        self.setDragDropMode(QListWidget.InternalMove)
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.accept()
+        else:
+            event.ignore()
+
+    def dragMoveEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.setDropAction(Qt.CopyAction)
+            event.accept()
+        else:
+            event.ignore()
+
+    def dropEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.setDropAction(Qt.CopyAction)
+            event.accept()
+            links = []
+            for url in event.mimeData().urls():
+                if url.isLocalFile():
+                    links.append(str(url.toLocalFile()))
+            self.addItems(links)
+        else:
+            event.ignore()
 
 
 class MainWindow(QMainWindow):
@@ -22,43 +58,56 @@ class MainWindow(QMainWindow):
 
         layout = QVBoxLayout()
 
-        self.status_label = QLabel(
-            "Select videos and destination to start upscaling.", self
-        )
-        layout.addWidget(self.status_label)
+        self.file_list = FileListWidget()
+        layout.addWidget(self.file_list)
 
-        self.upscale_button = QPushButton("Select Videos and Upscale", self)
+        self.progress_bar = QProgressBar(self)
+        layout.addWidget(self.progress_bar)
+
+        self.upscale_button = QPushButton("Upscale Videos", self)
         self.upscale_button.clicked.connect(self.select_and_upscale_video)
         layout.addWidget(self.upscale_button)
+
+        self.status_label = QLabel("Ready", self)
+        layout.addWidget(self.status_label)
 
         central_widget = QWidget()
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
 
     def select_and_upscale_video(self):
-        # Select Video Files
-        file_dialog = QFileDialog()
-        video_files, _ = file_dialog.getOpenFileNames(
-            self, "Select Video Files", "", "Video Files (*.mp4 *.avi *.mkv)"
-        )
+        if not self.file_list.count():
+            self.select_files()
 
-        # Select Destination Folder
-        destination_folder = file_dialog.getExistingDirectory(
+        destination_folder = QFileDialog.getExistingDirectory(
             self, "Select Destination Folder"
         )
-
-        if video_files and destination_folder:
-            self.video_processor = VideoProcessor(destination_folder, video_files)
-            self.video_processor.run()
+        if self.file_list.count() and destination_folder:
+            video_files = [
+                self.file_list.item(i).text() for i in range(self.file_list.count())
+            ]
+            for video_file in video_files:
+                processor = VideoProcessor(destination_folder, [video_file])
+                processor.progress_updated.connect(self.update_progress)
+                processor.start()
+                processor.wait()  # Wait for the thread to finish
             self.status_label.setText("Upscaling Completed.")
         else:
             self.status_label.setText("No videos selected or destination not set.")
 
+    def update_progress(self, progress):
+        self.progress_bar.setValue(progress)
+
+    def select_files(self):
+        file_dialog = QFileDialog()
+        video_files, _ = file_dialog.getOpenFileNames(
+            self, "Select Video Files", "", "Video Files (*.mp4 *.avi *.mkv)"
+        )
+        for file in video_files:
+            self.file_list.addItem(file)
+
 
 if __name__ == "__main__":
-    from PyQt5.QtWidgets import QApplication
-    import sys
-
     app = QApplication(sys.argv)
     main_window = MainWindow()
     main_window.show()
--- video_processor.py
+++ video_processor.py
@@ -1,113 +1,35 @@
-import subprocess
 import os
-import shutil
-import re
 import logging
-from typing import List
-
-# Initialize logging
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
-)
+from PyQt5.QtCore import QObject, pyqtSignal
+from ffmpeg_integration import FFmpegHandler
+from esrgan_integration import ESRGANHandler
 
 
-class VideoProcessor:
+class VideoProcessor(QObject):
+    progress_updated = pyqtSignal(int)
+
     def __init__(self, destination_folder: str, video_files: List[str]):
+        super().__init__()
         self.destination_folder = destination_folder
         os.makedirs(self.destination_folder, exist_ok=True)
         self.video_files = video_files
-
-    @staticmethod
-    def run_subprocess(
-        command: List[str], capture_output: bool = False
-    ) -> subprocess.CompletedProcess:
-        try:
-            return subprocess.run(
-                command, text=True, capture_output=capture_output, check=True
-            )
-        except subprocess.CalledProcessError as e:
-            logging.error(f"Error running command {' '.join(command)}: {e}")
-            return None
-
-    @staticmethod
-    def get_frame_rate(video_file: str) -> str:
-        result = VideoProcessor.run_subprocess(
-            ["ffmpeg", "-i", video_file], capture_output=True
-        )
-        if result and result.stderr:
-            matches = re.search(r"(\d{1,3}\.\d{1,2}) fps", result.stderr)
-            return matches.group(1) if matches else "25"
-        return "25"
+        self.ffmpeg_handler = FFmpegHandler()
+        self.esrgan_handler = ESRGANHandler()
 
     def process_video(self, video_file: str):
         logging.info(f"Processing {video_file}...")
-        frame_folder = self.extract_frames(video_file)
-        self.upscale_frames(frame_folder)
-        self.reassemble_video(video_file, frame_folder)
-
-    def extract_frames(self, video_file: str) -> str:
-        frame_folder = os.path.join(self.destination_folder, "temporary_frames")
-        os.makedirs(frame_folder, exist_ok=True)
-        frame_rate = self.get_frame_rate(video_file)
-        ffmpeg_command = [
-            "ffmpeg",
-            "-i",
-            video_file,
-            "-vf",
-            f"fps=fps={frame_rate}",
-            "-q:v",
-            "2",
-            os.path.join(frame_folder, "frame_%04d.jpg"),
-        ]
-        self.run_subprocess(ffmpeg_command)
-        return frame_folder
-
-    def upscale_frames(self, frame_folder: str):
-        output_folder = os.path.join(os.path.dirname(frame_folder), "out_frames")
-        os.makedirs(output_folder, exist_ok=True)
-        realesrgan_executable = os.path.join(
-            "Real-ESRGAN", "realesrgan", "realesrgan-ncnn-vulkan.exe"
+        frame_folder = self.ffmpeg_handler.extract_frames(
+            video_file, self.destination_folder
         )
-        self.run_subprocess(
-            [
-                realesrgan_executable,
-                "-i",
-                frame_folder,
-                "-o",
-                output_folder,
-                "-n",
-                "realesr-animevideov3",
-                "-s",
-                "2",
-                "-f",
-                "png",
-            ]
+        self.esrgan_handler.upscale_frames(frame_folder)
+        self.ffmpeg_handler.reassemble_video(
+            video_file, frame_folder, self.destination_folder
         )
-
-    def reassemble_video(self, original_video: str, frame_folder: str):
-        upscaled_video = os.path.join(
-            self.destination_folder, "upscaled_" + os.path.basename(original_video)
-        )
-        frame_rate = self.get_frame_rate(original_video)
-        ffmpeg_command = [
-            "ffmpeg",
-            "-i",
-            os.path.join(frame_folder, "frame_%04d.png"),
-            "-i",
-            original_video,
-            "-c:v",
-            "h264_nvenc",
-            "-preset",
-            "fast",
-            "-r",
-            frame_rate,
-            "-pix_fmt",
-            "yuv420p",
-            upscaled_video,
-        ]
-        self.run_subprocess(ffmpeg_command)
-        shutil.rmtree(frame_folder)
+        os.rmdir(frame_folder)
 
     def run(self):
-        for video_file in self.video_files:
+        total_videos = len(self.video_files)
+        for index, video_file in enumerate(self.video_files):
             self.process_video(video_file)
+            progress = int((index + 1) / total_videos * 100)
+            self.progress_updated.emit(progress)
