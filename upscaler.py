c1b977098368df2cd3f08f3dc5a94eba3a220757
JosepOli <josepolive89@gmail.com>
2023-12-27 18:20:48 +0100

    Started from scratch

--- upscaler.py
+++ upscaler.py
@@ -1,287 +1,258 @@
-import tkinter as tk
-from tkinter import filedialog, simpledialog, messagebox
-import subprocess
 import os
-import shutil
+import subprocess
+import tkinter as tk
+from tkinter import ttk, messagebox
+from tkinterdnd2 import DND_FILES, TkinterDnD
 import json
-import concurrent.futures
-
-# Set up the root Tkinter instance
-root = tk.Tk()
-root.withdraw()
-
-# -----------------------------
-# Helper Functions
-# -----------------------------
 
 
-def get_video_properties(video_file):
+# Function to get frame rate from a video using ffprobe
+def get_frame_rate(video_path):
     try:
-        cmd = f'ffprobe -v error -select_streams v:0 -show_entries stream=width,height,r_frame_rate -of json "{video_file}"'
+        cmd = [
+            "ffprobe",
+            "-v",
+            "error",
+            "-select_streams",
+            "v:0",
+            "-show_entries",
+            "stream=r_frame_rate",
+            "-of",
+            "json",
+            video_path,
+        ]
         result = subprocess.run(
-            cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+            cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
         )
-        if result.stderr:
-            print("Error in ffprobe:", result.stderr)
-            return None
-        return json.loads(result.stdout)
+        frame_rate_info = json.loads(result.stdout)
+        frame_rate = eval(frame_rate_info["streams"][0]["r_frame_rate"])
+        return frame_rate
     except Exception as e:
-        print("Error getting video properties:", e)
-        return None
+        print(f"Error calculating frame rate: {e}")
+        return 1
 
 
-def select_files_or_folder():
-    file_path = filedialog.askopenfilename(
-        title="Select a Video File", filetypes=[("Video files", "*.mp4 *.avi *.mkv")]
-    )
-    if file_path:
-        return [file_path]
-    folder_path = filedialog.askdirectory(title="Select Folder Containing Videos")
-    if folder_path:
-        return [
-            os.path.join(folder_path, f)
-            for f in os.listdir(folder_path)
-            if f.endswith((".mp4", ".avi", ".mkv"))
-        ]
-    return []
-
-
-def select_destination_folder():
-    folder_path = filedialog.askdirectory(
-        title="Select Destination Folder for Upscaled Videos"
-    )
-    return folder_path if folder_path else "upscaled_videos"
-
-
-def list_upscaling_models(models_folder):
-    if not os.path.exists(models_folder):
-        raise FileNotFoundError(
-            f"The specified models folder does not exist: {models_folder}"
-        )
-    return [f for f in os.listdir(models_folder) if f.endswith(".bin")]
-
-
-def user_select_model(models):
-    def on_select(evt):
-        selection = listbox.get(listbox.curselection())
-        user_choice.set(selection)  # Set the user's choice
-
-    user_choice = tk.StringVar(root)  # Variable to store the user's choice
-    dialog = tk.Toplevel(root)
-    dialog.title("Select Upscaling Model")
-
-    listbox = tk.Listbox(dialog, width=50, height=20)
-    listbox.pack(side="left", fill="both", expand=True)
-
-    scrollbar = tk.Scrollbar(dialog, orient="vertical")
-    scrollbar.config(command=listbox.yview)
-    scrollbar.pack(side="right", fill="y")
-
-    listbox.config(yscrollcommand=scrollbar.set)
-    for item in models:
-        listbox.insert(tk.END, item)
-
-    listbox.bind("<<ListboxSelect>>", on_select)
-
-    ok_button = tk.Button(dialog, text="OK", command=dialog.destroy)
-    ok_button.pack()
-
-    root.wait_window(dialog)  # Wait for the user to make a selection
-    return user_choice.get()  # Return the user's choice
-
-
-# -----------------------------
-# Video Processing Functions
-# -----------------------------
-
-
-def extract_and_upscale_frames(
-    video_file, destination_folder, frame_rate, scale_factor, model
-):
-    frame_folder = None
+# Function to extract frames from a video using ffmpeg
+def extract_frames(video_path, dest_folder, fps):
+    if not os.path.exists(dest_folder):
+        os.makedirs(dest_folder)
+    command = [
+        "ffmpeg",
+        "-i",
+        video_path,
+        "-vf",
+        f"fps={fps}",
+        os.path.join(dest_folder, "frame_%04d.png"),
+    ]
     try:
-        frame_folder = os.path.join(
-            destination_folder, os.path.basename(video_file) + "_frames"
-        )
-        os.makedirs(frame_folder, exist_ok=True)
-
-        output_folder = os.path.join(
-            destination_folder, os.path.basename(video_file) + "_upscaled"
-        )
-        os.makedirs(output_folder, exist_ok=True)
-
-        # Adjusted FFmpeg command
-        ffmpeg_command = [
-            "ffmpeg",
-            "-i",
-            video_file,
-            "-vf",
-            f"fps={frame_rate},scale=1280:720:flags=lanczos",
-            "-threads",
-            "0",
-            os.path.join(frame_folder, "frame_%04d.jpg"),
-        ]
-
-        print("Running FFmpeg command for frame extraction:", " ".join(ffmpeg_command))
-        # Run FFmpeg without capturing the output to see progress in the console
-        subprocess.run(ffmpeg_command, check=True)
-
-        # If realesrgan-ncnn-vulkan is not in PATH, provide the full path to the executable
-        realesrgan_executable = r"realesrgan"  # Update this to the actual path
-        output_folder = os.path.join(
-            destination_folder, os.path.basename(video_file) + "_upscaled"
-        )
-        os.makedirs(output_folder, exist_ok=True)
-
-        # Make sure the paths are correct
-        input_frames_path = os.path.join(frame_folder, "frame_%04d.jpg")
-        if not os.path.exists(input_frames_path.format(1)):
-            raise FileNotFoundError(f"No frames found in {frame_folder}")
-
-        realesrgan_command = [
-            realesrgan_executable,
-            "-i",
-            frame_folder,
-            "-o",
-            output_folder,
-            "-n",
-            model,
-            "-s",
-            str(scale_factor),
-        ]
-
-        # Run Real-ESRGAN without capturing the output to see progress in the console
-        subprocess.run(realesrgan_command, check=True)
-
-        return output_folder
+        subprocess.run(command, check=True)
+        print(f"Frames extracted to {dest_folder}")
     except subprocess.CalledProcessError as e:
-        print(
-            f"Error in subprocess: {e.stderr.decode() if e.stderr else 'Unknown error'}"
-        )
-        return None
-    except Exception as e:
-        print(f"Error in extract_and_upscale_frames: {e}")
-        return None
-    finally:
-        if frame_folder and os.path.exists(frame_folder):
-            shutil.rmtree(frame_folder)
+        print(f"An error occurred: {e}")
 
 
-def reassemble_video(original_video, frame_folder, destination_folder, frame_rate):
+# Upscaling images using Real-ESRGAN
+def upscale_image(input_path, output_path, model, progress_callback):
+    cmd = [
+        "realesrgan-ncnn-vulkan.exe",
+        "-i",
+        input_path,
+        "-o",
+        output_path,
+        "-m",
+        "realesrgan/models",
+        "-n",
+        model,
+    ]
     try:
-        upscaled_video = os.path.join(
-            destination_folder, "upscaled_" + os.path.basename(original_video)
-        )
-        ffmpeg_command = [
-            "ffmpeg",
-            "-hwaccel",
-            "cuda",
-            "-hwaccel_output_format",
-            "cuda",
-            "-framerate",
-            frame_rate,
-            "-i",
-            os.path.join(frame_folder, "frame_%04d.png"),
-            "-i",
-            original_video,
-            "-c:v",
-            "h264_nvenc",
-            "-preset",
-            "fast",
-            "-r",
-            frame_rate,
-            "-pix_fmt",
-            "yuv420p",
-            upscaled_video,
-        ]
-        subprocess.run(ffmpeg_command, check=True, stderr=subprocess.PIPE)
+        subprocess.run(cmd, check=True)
+        progress_callback(1)  # Update progress
     except subprocess.CalledProcessError as e:
-        print("Error in subprocess:", e.stderr.decode())
-    except Exception as e:
-        print("Error in reassemble_video:", e)
+        print(f"An error occurred during upscaling: {e}")
 
 
-def process_video(video_file, destination_folder, frame_rate, scale_factor, model):
+# Function to reassemble video with original audio
+def reassemble_video(frame_folder, audio_file, output_video, frame_rate):
+    cmd = [
+        "ffmpeg",
+        "-r",
+        str(frame_rate),
+        "-i",
+        os.path.join(frame_folder, "frame_%04d.png"),
+        "-i",
+        audio_file,
+        "-c:v",
+        "libx264",
+        "-c:a",
+        "copy",
+        "-strict",
+        "experimental",
+        output_video,
+    ]
     try:
-        print(f"Processing {video_file}...")
-        upscaled_frames_folder = extract_and_upscale_frames(
-            video_file, destination_folder, frame_rate, scale_factor, model
-        )
-        reassemble_video(
-            video_file, upscaled_frames_folder, destination_folder, frame_rate
-        )
-        shutil.rmtree(upscaled_frames_folder)
-    except Exception as e:
-        print("Error in process_video:", e)
+        subprocess.run(cmd, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred during video reassembly: {e}")
 
 
-# -----------------------------
-# Main Function
-# -----------------------------
+# Function to update the progress bar
+def update_progress(n):
+    progress["value"] += n
+    root.update_idletasks()
 
 
-def main():
-    print("Starting script...")  # Diagnostic print
+def populate_models_dropdown():
+    models_path = "realesrgan/models"
     try:
-        print("Selecting destination folder...")  # Diagnostic print
-        destination_folder = select_destination_folder()
-        print(f"Destination folder: {destination_folder}")  # Diagnostic print
-        os.makedirs(destination_folder, exist_ok=True)
-
-        print("Selecting video files...")  # Diagnostic print
-        video_files = select_files_or_folder()
-        print(f"Selected video files: {video_files}")  # Diagnostic print
-
-        models_folder = os.path.abspath("realesrgan/models")
-        models = list_upscaling_models(models_folder)
-        print(f"Available models: {models}")  # Diagnostic print
-
-        model = user_select_model(models)
-        if not model:
-            messagebox.showinfo("Info", "No model selected. Exiting.")
-            return
-
-        scale_factor = simpledialog.askstring(
-            "Input", "Enter scale factor (e.g., 2):", parent=root
-        )
-        print(f"Selected scale factor: {scale_factor}")  # Diagnostic print
-
-        with concurrent.futures.ProcessPoolExecutor() as executor:
-            futures = []
-            for video_file in video_files:
-                video_properties = get_video_properties(video_file)
-                if (
-                    video_properties
-                    and "streams" in video_properties
-                    and len(video_properties["streams"]) > 0
-                ):
-                    frame_rate = video_properties["streams"][0]["r_frame_rate"]
-                    output_folder = os.path.join(
-                        destination_folder, os.path.basename(video_file) + "_upscaled"
-                    )
-                    os.makedirs(output_folder, exist_ok=True)
-                    future = executor.submit(
-                        process_video,
-                        video_file,
-                        destination_folder,
-                        frame_rate,
-                        scale_factor,
-                        model,
-                    )
-                    futures.append(future)
-                    print(f"Processing started for: {video_file}")
-                else:
-                    print(f"Failed to get video properties for {video_file}")
-
-            concurrent.futures.wait(futures)
-            print("Processing complete for all videos.")
-    except FileNotFoundError as e:
-        messagebox.showerror("File Not Found Error", str(e))
+        model_files = [f for f in os.listdir(models_path) if f.endswith(".bin")]
+        return model_files
     except Exception as e:
-        messagebox.showerror("Error", str(e))
-    finally:
-        root.destroy()
+        print(f"Error accessing models directory: {e}")
+        return []
 
 
-if __name__ == "__main__":
-    main()
+def get_audio_bitrate(video_path):
+    try:
+        cmd = [
+            "ffprobe",
+            "-v",
+            "error",
+            "-select_streams",
+            "a:0",
+            "-show_entries",
+            "stream=bit_rate",
+            "-of",
+            "default=noprint_wrappers=1:nokey=1",
+            video_path,
+        ]
+        result = subprocess.run(
+            cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+        )
+        bitrate = int(result.stdout.strip())
+        return bitrate
+    except Exception as e:
+        print(f"Error getting audio bitrate: {e}")
+        return None  # Return None or a default value if you prefer
+
+
+def on_drop(event):
+    video_files_raw = event.data.strip().split()
+    # Clean file path and reconstruct paths considering spaces and quotes
+    video_files = [
+        file.replace("{", "").replace("}", "").replace("file:///", "")
+        for file in video_files_raw
+    ]
+    video_files = " ".join(video_files)
+    video_files = video_files.split(" '")
+    video_files = [file.strip("'") for file in video_files]
+
+    for video_file in video_files:
+        video_file = video_file.replace("/", "\\")
+        if not os.path.isabs(video_file):
+            video_file = f"C:{video_file}"
+
+        try:
+            print(f"Processing: {video_file}")
+            # Get video frame rate and original audio
+            frame_rate = get_frame_rate(video_file)
+            filename = os.path.basename(video_file)
+            basename = os.path.splitext(filename)[0]
+            output_folder = f"extracted_frames_{basename}"
+            audio_temp = f"temp_audio_{basename}.aac"  # Temp file for audio
+
+            # Extract frames
+            extract_frames(video_file, output_folder, frame_rate)
+
+            original_bitrate = get_audio_bitrate(video_file)
+            audio_bitrate_option = (
+                f"-b:a {original_bitrate}k" if original_bitrate else ""
+            )  # Use original bitrate if available
+
+            # Extract audio with the determined bitrate
+            cmd_audio = [
+                "ffmpeg",
+                "-i",
+                video_file,
+                "-vn",  # No video
+                "-acodec",
+                "aac",  # Re-encode audio to AAC
+                audio_bitrate_option,  # Apply bitrate
+                audio_temp,
+            ]
+            try:
+                subprocess.run(cmd_audio, check=True)
+            except subprocess.CalledProcessError as e:
+                print(f"An error occurred during audio extraction: {e}")
+
+            # Upscale frames
+            frames_folder = os.path.join(output_folder, "frames")
+            upscaled_folder = os.path.join(output_folder, "upscaled")
+            if not os.path.exists(upscaled_folder):
+                os.makedirs(upscaled_folder)
+            frame_files = [
+                os.path.join(frames_folder, f)
+                for f in os.listdir(frames_folder)
+                if f.endswith(".png")
+            ]
+
+            total_frames = len(frame_files)
+            progress["maximum"] = total_frames
+
+            for idx, frame in enumerate(frame_files):
+                output_frame = os.path.join(upscaled_folder, f"upscaled_{idx:04d}.png")
+                upscale_image(
+                    frame,
+                    output_frame,
+                    model_var.get(),
+                    lambda x: update_progress(x / total_frames),
+                )
+                print(
+                    f"Upscaled {idx + 1}/{total_frames} frames"
+                )  # Optional verbose output
+
+            # Reassemble video with upscaled frames and original audio
+            output_video = f"upscaled_{basename}.mp4"
+            reassemble_video(upscaled_folder, audio_temp, output_video, frame_rate)
+
+            # Cleanup
+            if messagebox.askyesno(
+                "Cleanup", "Do you want to delete the temporary files?"
+            ):
+                os.remove(audio_temp)
+                for f in os.listdir(frames_folder):
+                    os.remove(os.path.join(frames_folder, f))
+                for f in os.listdir(upscaled_folder):
+                    os.remove(os.path.join(upscaled_folder, f))
+                os.rmdir(frames_folder)
+                os.rmdir(upscaled_folder)
+
+            print(f"Finished processing {video_file}")
+        except Exception as e:
+            print(f"Failed to process {video_file}: {e}")
+            # Optional cleanup or error handling
+
+
+# Tkinter GUI setup
+root = TkinterDnD.Tk()
+root.title("Drag and Drop Videos Here")
+
+# Dropdown for model selection
+model_var = tk.StringVar()
+model_dropdown = ttk.Combobox(
+    root, textvariable=model_var, values=populate_models_dropdown()
+)
+model_dropdown.pack()
+
+# Label for drag and drop
+label = tk.Label(root, text="Drag and drop video files here", padx=100, pady=100)
+label.pack(fill=tk.BOTH, expand=True)
+
+# Progress Bar
+progress = ttk.Progressbar(root, orient=tk.HORIZONTAL, length=100, mode="determinate")
+progress.pack()
+
+# Bind drop event and set up the Tkinter DnD window
+label.drop_target_register(DND_FILES)
+label.dnd_bind("<<Drop>>", on_drop)
+
+root.mainloop()
